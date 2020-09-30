0735f6b7f63282147081037aac24901ce0104cb7
ips-coding-challenge <coding_challenge@laposte.net>
2020-09-30 13:44:34 +0200

    Initialize project using Create React App

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.js',
+    './src/**/*.jsx',
+    './src/**/*.ts',
+    './src/**/*.tsx',
+    './public/index.html',
+  ],
+  theme: {
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+}
